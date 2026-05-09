32ba7fda620691071fb3d31621bb495a939d6ecb
itsmebilly004 <wanyamabill4@gmail.com>
2026-05-09 15:47:53 +0300

    t

--- src/lib/deriv.ts
+++ src/lib/deriv.ts
@@ -1,6 +1,6 @@
 // src/lib/deriv.ts
 
-const DERIV_APP_ID = import.meta.env.VITE_DERIV_APP_ID || "33dF8d2wwjIpeFDBvNkln";
+const DERIV_APP_ID = import.meta.env.VITE_DERIV_APP_ID || "133647";
 const WS_URL = `wss://ws.derivws.com/websockets/v3?app_id=${DERIV_APP_ID}&l=EN`;
 
 export const DERIV_APP_ID_VALUE = DERIV_APP_ID;
